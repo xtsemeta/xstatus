1608360a8e7891b3f9b4058e83d877bf7730f0c0
lala <lala@laladeMacBook-Pro-2.local>
2022-01-20 14:28:41 +0800

    init

---
+++ sample/src/main/java/com/jaeger/statusbarutil/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.jaeger.statusbarutil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import com.jaeger.library.StatusBarUtil;
+import com.jaeger.statusbardemo.R;
+
+/**
+ * Created by Jaeger on 16/2/14.
+ *
+ * Email: chjie.jaeger@gmail.com
+ * GitHub: https://github.com/laobie
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setStatusBar();
+    }
+
+    protected void setStatusBar() {
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
